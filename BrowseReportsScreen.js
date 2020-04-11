5de31e5320908dfd02386740e2633f0cddee6338
Madhav Jaisankar <jaisanka@sheridancollege.ca>
2020-04-10 23:49:57 -0400

    made a details screen which displays the details of a selected report in the BrowseReportsScreen.js

--- BrowseReportsScreen.js
+++ BrowseReportsScreen.js
@@ -18,9 +18,55 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 var reportArray = [];
 
+function showFlatList({navigation}){
+    return(
+        <FlatList
+            numColumns={1}
+            data={reportArray}
+            renderItem={({ item, index }) => (
+                <TouchableOpacity onPress={() => navigation.navigate('DetailsScreen', { reportObj: item } )}>
+                    <View style={styles.item}>
+                        <Text  style={styles.title}>{item.idNum + 1}. {item.title}</Text>
+                        <Text style={styles.subTitle}>{item.date}</Text>
+                    </View>
+                </TouchableOpacity>
+
+            )}
+            keyExtractor={item => item.idNum}
+        />
+    );
+}
+
+
+
+function showDetailsScreen({route, navigation}){
+    const {reportObj} = route.params;
+    return(
+        <View>
+            <Text>Title: {reportObj.title}</Text>
+            <Text>{reportObj.date}</Text>
+            <Text>Description: {reportObj.desc}</Text>
+            <Image
+                style={{ height: 50, width: 50, marginVertical: 5 }}
+                source={{uri: reportObj.image}}
+            />
+            <Text>Location of incident: {reportObj.location}</Text>
+            <Text>Make and Model: {reportObj.model}</Text>
+            <Text>License plate number of perpetrator: {reportObj.plateNumber}</Text>
+            <Text>Upvotes: {reportObj.upvotes}</Text>
+            <Button title={"Upvote"} onPress={() => 5} />
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
+
 export default class BrowseScreen extends React.Component {
 
     componentWillMount() {
+
+        reportArray = [];
+
         const firebaseConfig = {
             apiKey: 'AIzaSyCiS4hoJgPXTRClfOUI-dBQ6hPkdgohqdc',
             authDomain: 'dreadful-drivers.firebaseapp.com',
@@ -47,31 +93,18 @@ export default class BrowseScreen extends React.Component {
         });
     }
 
+
     render() {
         console.log(JSON.stringify(reportArray));
         return (
-            // <View style={styles.container}>
-                <FlatList
-                    numColumns={1}
-                    data={reportArray}
-                    renderItem={({ item, index }) => (
-                        <TouchableOpacity onPress={() => this.selectReport()}>
-                            <View style={styles.item}>
-                                <Text  style={styles.title}>{item.idNum + 1}. {item.title}</Text>
-                                <Text style={styles.subTitle}>{item.date}</Text>
-                            </View>
-                        </TouchableOpacity>
-
-                    )}
-                    keyExtractor={item => item.idNum}
-                />
-            // </View>
+                <Stack.Navigator initialRouteName={"ReportsList"} screenOptions={{headerShown: false}}>
+                    <Stack.Screen name={"ReportsList"} component={showFlatList}/>
+                    <Stack.Screen name={"DetailsScreen"} component={showDetailsScreen}/>
+                </Stack.Navigator>
         );
     }
 
-    selectReport = () => {
-        Alert.alert("Clicked");
-    }
+
 }
 
 const styles = StyleSheet.create({
